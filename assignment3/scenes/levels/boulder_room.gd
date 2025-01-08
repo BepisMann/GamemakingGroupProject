eadce9eac875619b6320b75dfc430f861737e37a
Pechi Vladimir-Ștefan <pechi.vladimir2002@gmail.com>
2025-01-09 00:30:36 +0100

    Fix ceiling breaking sound

--- assignment3/scenes/levels/boulder_room.gd
+++ assignment3/scenes/levels/boulder_room.gd
@@ -18,7 +18,7 @@ func _on_boulder_drop_area_body_entered(body: Node3D) -> void:
 	if body.name == "Player" and !wall_deleted:
 		body.set_spawn_point_2()
 		wall_deleted = true
-		$"Hallway/Ceiling_Breakable/AudioStreamPlayer3D".play()
+		$Hallway/BoulderTrapActivated.play()
 		$Hallway/Ceiling_Breakable.queue_free()
 
 
@@ -28,7 +28,3 @@ func _on_boulder_player_crushed() -> void:
 
 func _on_trap_body_entered_spikes() -> void:
 	emit_signal("playerDied")
-
-
-func _on_audio_stream_player_3d_finished() -> void:
-	queue_free()
