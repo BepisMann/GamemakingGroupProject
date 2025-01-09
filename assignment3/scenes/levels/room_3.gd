97709bf84b57bb232341b6aed74c631fb272c695
Pechi Vladimir-Ștefan <pechi.vladimir2002@gmail.com>
2025-01-09 23:39:39 +0100

    Restart chess button

--- assignment3/scenes/levels/room_3.gd
+++ assignment3/scenes/levels/room_3.gd
@@ -12,7 +12,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+func reset_chess() -> void:
+	$ChessRoom/ChessRoom.reset()
+	await get_tree().create_timer(0.5).timeout
+	$ChessRoom/RestartButton.is_pressed = false
+	
 func _on_player_piece_touched(body: Object) -> void:
 	emit_signal("piece_touched", body)
 
@@ -23,4 +27,5 @@ func _on_player_piece_moved(body: Object) -> void:
 
 func _on_chess_room_player_won_chess() -> void:
 	$ChessRoom/ChessRoom.queue_free()
+	$ChessRoom/RestartButton.queue_free()
 	$ChessRoom/Letter_before_solution/Letter_of_translation.reparent($ChessRoom/Letter_of_translation_position, false)
